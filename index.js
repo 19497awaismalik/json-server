bbc3c6c78fc377a5b8117848dc12362e2f9613d8
19497awaismalik <19497.awaismalik@gmail.com>
2024-04-12 23:08:33 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const jsonServer = require('json-server');
+const cors=require("cors")
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
+
+
+const PORT = process.env.PORT || 3006;
+
+server.options("",cors({
+    origin:"*",
+    credentials:true,
+    methods:["GET","POST","PUT","DELELTE"]
+}))
+server.use(cors({
+    origin:"*",
+    credentials:true,
+    methods:["GET","POST","PUT","DELELTE"]
+}))
+server.use(middlewares);
+server.use(router);
+
+server.listen(PORT, () => {
+  console.log(`JSON Server is running on port ${PORT}`);
+});
